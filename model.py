a4db67c3328cef1cae55f0b81260ea844630c4b7
Joong Gon Yim <joonggonyim@gmail.com>
2017-06-11 17:10:24 -0700

    cv.imread too slow. \n plan to load all images and save to pickle first

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ import numpy as np
 import sklearn
 from sklearn.model_selection import train_test_split
 
+import time
 # model.py
 
 # TODO LIST
@@ -13,7 +14,7 @@ from sklearn.model_selection import train_test_split
 # - Output a new steering angle for the car.
 
 current_file_path = os.path.dirname(os.path.realpath(__file__))
-data_path_list = [os.path.join(current_file_path,"data","data"), os.path.join(current_file_path,"data_my_driving")]
+data_path_list = [os.path.join(current_file_path,"data","data")]#, os.path.join(current_file_path,"data_my_driving")]
 
 
 
@@ -22,13 +23,14 @@ def load_data(data_path):
     csvpath = os.path.join(data_path,"driving_log.csv")
     with open(csvpath) as csvfile:
         reader = csv.reader(csvfile)
+        next(reader)
         for line in reader:
             for ii,l in enumerate(line[:3]):
                 line[ii] = os.path.join(data_path,"IMG",os.path.basename(l))
                 line_mod = [line[ii]] + line[3:]
                 samples.append(line_mod)
 
-    train_samples, validation_samples = train_test_split(samples[1:], test_size=0.2)
+    train_samples, validation_samples = train_test_split(samples, test_size=0.2)
     return train_samples, validation_samples
 
 
@@ -39,6 +41,7 @@ for data_path in data_path_list:
     train_samples += train_samples_tmp
     validation_samples += validation_samples_tmp
 
+print(len(train_samples))
 
 def generator(samples, angle_correction=0.2,batch_size=32):
     num_samples = len(samples)
@@ -48,18 +51,22 @@ def generator(samples, angle_correction=0.2,batch_size=32):
             batch_samples = samples[offset:offset+batch_size]
             images = []
             angles = []
+            t_read = 0
             for batch_sample in batch_samples:
                 path = batch_sample[0]
                 angle = batch_sample[1]
+                t0 = time.time()
                 center_image = cv2.imread(path)
+                t_read+=(time.time() - t0)
                 center_angle = float(angle)
                 if "left_" in path:
                     center_angle += angle_correction
                 elif "right_": # right
                     center_angle -= angle_correction
-
                 images.append(center_image)
                 angles.append(center_angle)
+            
+            print(t_read)
 
             # trim image to only see section with road
             X_train = np.array(images)
@@ -67,10 +74,12 @@ def generator(samples, angle_correction=0.2,batch_size=32):
             yield sklearn.utils.shuffle(X_train, y_train)
 
 # compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=32)
-validation_generator = generator(validation_samples, batch_size=32)
+batch_size = 256
+train_generator = generator(train_samples, batch_size=batch_size)
+validation_generator = generator(validation_samples, batch_size=batch_size)
 
-x,y = next(train_generator)
+
+next(train_generator)
 
 ch, row, col = 3, 80, 320  # Trimmed image format
 
@@ -79,6 +88,9 @@ input_shape = (160,320,3)
 # ---- import from keras ----
 from keras.models import Sequential, Model
 from keras.layers import Cropping2D, Input, Flatten, Dense, Lambda
+from keras.layers import Activation, Dropout
+from keras.layers import Conv2D,MaxPooling2D
+# ---------------------------
 
 def model_printer(model):
     line_breaker = "+{:-<22}+{:-<22}+".format("","")
@@ -101,16 +113,65 @@ def model_builder(crop_dim,input_shape,print_shape=True):
     model.add(Lambda(lambda x: x/127.5 - 1.))
 
     # === build model ===
-    
+    # conv layer 1
+    model.add(Conv2D(16,(5,5),activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
+
+    # conv layer 2
+    model.add(Conv2D(32,(5,5),activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
+
+    # conv layer 3 
+    model.add(Conv2D(64,(5,5),activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
+
+    # Flatten the 4d tensor
+    model.add(Flatten())
+
+    # FC 1
+    model.add(Dense(5000,activation='relu'))
+    model.add(Dropout(0.25))
+
+    # FC2 
+    model.add(Dense(1024,activation='relu'))
+    model.add(Dropout(0.25))
+
+    # FC3
+    model.add(Dense(128,activation='relu'))
+    model.add(Dropout(0.25))
+
+    # FC4
+    model.add(Dense(1))
+    model.add(Dropout(0.25))
+
+
     model.compile(loss='mse', optimizer='adam')
 
     if print_shape: model_printer(model)
         
-
     return model
 
-model = model_builder(crop_dim,input_shape)
-# print([_.output.name.split('/')[0] for _ in model.layers])
+
+def simple_model_builder(input_shape,print_shape=True):
+    model = Sequential()
+    model.add(Flatten(input_shape=input_shape))
+
+    model.add(Dense(1))
+
+    model.compile(loss='mse', optimizer='adam')
+
+    if print_shape: model_printer(model)
+        
+    return model
+
+
+# model = model_builder(crop_dim,input_shape)
+
+
+model = simple_model_builder(input_shape)
 # model.fit_generator(train_generator, samples_per_epoch=len(train_samples), \
 #     validation_data=validation_generator, \
 #     nb_val_samples=len(validation_samples), nb_epoch=3)
